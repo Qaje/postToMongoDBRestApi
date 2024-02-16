d00931605700dc7f457d716dfc46e9f6fbb014d0
qaje <javierpal3232@gmail.com>
2024-02-15 23:17:17 -0400

    commit proyect

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Form/> 
+    <Table/> 
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import Form from './components/Form.vue';
+import Table from "./components/Table.vue"
+</script>
+
+
+<style scoped></style>
